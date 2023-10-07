ae09f06d856fbaa5db9e0521e23566ef87384309
wiktor <nobis171@wp.pl>
2023-10-07 17:13:22 +0200

    add data to store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,16 @@
+import storeItems from "../data/items.json"
+import {Col, Row} from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem.tsx";
+
 function Store() {
     return (
-        <div>Store</div>
+        <Row md={2} xs={1} className="g-3">
+            {storeItems.map(item => (
+            <Col key={item.id}>
+                <StoreItem {...item} />
+            </Col>
+            ))}
+        </Row>
     );
 }
 
